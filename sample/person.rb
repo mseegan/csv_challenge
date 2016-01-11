b099d1f2a8c59ff3f12c14827f8db2f493c689a4
mseegan <mseegan@gmail.com>
2016-01-11 11:02:37 -0800

    set up person

---
+++ sample/person.rb
@@ -0,0 +1,10 @@
+class Person
+	attr_reader :last :first :gender :DOB :color
+	def initialize last first gender DOB color
+		@last = last
+		@first = first@gender = gender
+		@DOB = DOB
+		@color = color
+	end
+
+end
